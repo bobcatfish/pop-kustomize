9fe5769dab4b04da84b1dd984e1e0442841f4715
Christie <catw@google.com>
2023-08-03 22:27:40 +0000

    Setup triggered deployment
    Trigger on merge to main

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -1,9 +1,10 @@
+set -ex
 # Creates 3 GKE autopilot clusters
 # Initializes APIS, sets up the Google Cloud Deploy pipeline
 # bail if PROJECT_ID is not set
 if [[ -z "${PROJECT_ID}" ]]; then
   echo "The value of PROJECT_ID is not set. Be sure to run export PROJECT_ID=YOUR-PROJECT first"
-  return
+  exit 1
 fi
 # Test cluster
 echo "creating testcluster..."
